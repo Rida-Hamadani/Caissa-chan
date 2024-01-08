c81e0b45daf5a070fec63bbe4fe5f695f1c02964
Rida-Hamadani <mridahamadani@gmail.com>
2024-01-08 23:57:48 +0200

    implement basic functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const { Client, IntentsBitField } = require("discord.js");
+require("dotenv").config();
+const { Client, IntentsBitField, EmbedBuilder } = require("discord.js");
 
 const client = new Client({
   intents: [
@@ -15,9 +16,15 @@ client.on("ready", (c) => {
 
 client.on("messageCreate", (msg) => {
   if (msg.author.bot) return;
-  if (msg.content == "haro") {
-    msg.reply("hello " + msg.author.globalName);
+  if (msg.content.toLowerCase().startsWith("!fen")) {
+    const msgArray = msg.content.split(' ');
+    const turn = msgArray[2] == 'w' ? 'White to move' : 'Black to move';
+    const emb = new EmbedBuilder()
+      .setColor(0xBE4B57)
+      .setTitle(turn)
+      .setImage(`https://fen2png.com/api/?fen=${msgArray[1]}%20${msgArray[2]}%20-%20-%20-&raw=true`);
+    msg.reply({ embeds: [emb] });
   }
 });
 
-client.login(TOKEN);
+client.login(process.env.TOKEN);
