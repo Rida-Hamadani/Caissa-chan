218e2ea9e02e39dafc61ecfe3eed5d6e712152e2
Rida-Hamadani <mridahamadani@gmail.com>
2024-01-12 23:29:43 +0200

    fix fen validation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { Client, IntentsBitField, EmbedBuilder } = require("discord.js");
+const { validateFen } = require("chess.js");
 
 const client = new Client({
   intents: [
@@ -10,21 +11,26 @@ const client = new Client({
   ],
 });
 
-const regex = new RegExp("/^([1-8PNBRQK]+/){7}[1-8PNBRQK]+$/gim");
-
 client.on("messageCreate", (msg) => {
   if (msg.author.bot) return;
   if (msg.content.toLowerCase().startsWith("!fen")) {
     const msgArray = msg.content.split(" ");
     const emb = new EmbedBuilder();
-    if (msgArray.length < 3 || regex.test(msgArray[2])) {
+    const { ok, error } = validateFen(
+      msgArray[1] + " " + msgArray[2] + " - - 0 1"
+    );
+    if (
+      msgArray.length < 3 ||
+      !ok ||
+      !(msgArray[2] == "w" || msgArray[2] == "b")
+    ) {
       emb
         .setColor(0xbe4b57)
         .setTitle("Invalid FEN")
         .setURL(
           "https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation"
         )
-        .setDescription("Please enter a valid Fen, burunya!");
+        .setDescription("burunya! " + error);
     } else {
       const turn = msgArray[2] == "w" ? "White to move" : "Black to move";
       emb
